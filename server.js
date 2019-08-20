78eb70686b4d9303f1efe1e52af5c58aec26947b
Anuj Chitley <ac261997@gmail.com>
2019-08-20 18:44:05 +0530

    connected database

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res ,db) });
 
-app.put('/image', (req, res) => { Image.handleImage(req, res, db) })
-
 app.post('/imageurl', (req, res) => { image.handleApiCall(req,res) })
+
+app.put('/image', (req, res) => { Image.handleImage(req, res, db) })
 
 
 app.listen(process.env.PORT || 3000, () => {
